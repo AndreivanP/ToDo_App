0af04c0202fa2dced1d11e21b7fae4fcd011b32f
Andreivan <andreivan.santos@gmail.com>
2020-06-10 21:05:23 +0200

    Connect F/E with B/E JPA

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -1,27 +1,27 @@
 import axios from "axios"
 import AuthenticationService from "../../components/todo/AuthenticationService";
-import { API_URL } from "../../Properties"
+import { API_URL, JPA_API_URL } from "../../Properties"
 
 class TodoDataService {
     retrieveAllTodos(username, password) {
-        return axios.get(`${API_URL}/users/${username}/todos`, 
+        return axios.get(`${JPA_API_URL}/users/${username}/todos`, 
             {headers: {authorization: AuthenticationService.createBasicAuthToken(username, password)}});
     }
 
     deleteTodo(name, id) {
-        return axios.delete(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     retrieveTodo(name, id) {
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     updateTodo(name, id, todo) {
-        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+        return axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo);
     }
 
     createTodo(name, todo) {
-        return axios.post(`${API_URL}/users/${name}/todos`, todo);
+        return axios.post(`${JPA_API_URL}/users/${name}/todos`, todo);
     }
 }
 
